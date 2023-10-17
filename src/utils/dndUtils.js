dbc743263b102e41d020b1c14fbe001004fc5f18
Vas <vasylviter95@gmail.com>
2023-10-17 23:25:44 +0300

    dnd-kit done

---
+++ src/utils/dndUtils.js
@@ -0,0 +1,24 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export const SortableItem = (props) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <li
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="text-lg bg-gray-50 rounded-md px-4 py-2 mb-2 last-of-type:mb-0"
+    >
+      {props.id}
+    </li>
+  );
+};
